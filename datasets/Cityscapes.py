fix: update export folder to 'train', adjust preprocessing resize dimensions, and modify homography scaling parameters in configuration files

datasets/Cityscapes.py
@@ -257,22 +257,7 @@ class Cityscapes(data.Dataset):
             R_warped = R_delta @ R_cam
             t_warped = t_cam + t_delta
             H_np = compute_homography(K, R_cam, t_cam, R_warped, t_warped)
-
-            def scale_homography(H, H_src, W_src, H_dst, W_dst):
-                """
-                Scale a homography from original image size to resized image size.
-                """
-                S = np.array([
-                    [W_dst / W_src, 0, 0],
-                    [0, H_dst / H_src, 0],
-                    [0, 0, 1]
-                ])
-                H_scaled = S @ H @ np.linalg.inv(S)
-                return H_scaled
-
-            # Use original Cityscapes dimensions
-            H_np = scale_homography(H_np, H_src=1024, W_src=2048, H_dst=H, W_dst=W)
-
+            # print(f"[DEBUG] Homography matrix:\n{H_np}")
             # print("H_np:\n", H_np)
 
             H_tensor = torch.tensor(H_np, dtype=torch.float32)

datasets/Coco.py
@@ -12,6 +12,7 @@ import cv2
 from utils.utils import homography_scaling_torch as homography_scaling
 from utils.utils import filter_points
 from pycocotools.coco import COCO
+from utils.utils import compute_valid_mask
 
 class Coco(data.Dataset):
     """COCO dataset loader.
@@ -135,7 +136,7 @@ class Coco(data.Dataset):
         self.sample_homography = sample_homography
         self.inv_warp_image = inv_warp_image
         self.inv_warp_image_batch = inv_warp_image_batch
-        self.valid_mask_result = compute_valid_mask
+        self.compute_valid_mask = compute_valid_mask
         self.ImgAugTransform = ImgAugTransform
         self.customizedTransform = customizedTransform
         self.warp_points = warp_points
@@ -289,7 +290,8 @@ class Coco(data.Dataset):
         # img_aug = _preprocess(img_aug[:,:,np.newaxis])
         img_aug = torch.tensor(img_aug, dtype=torch.float32).view(-1, H, W)
 
-        valid_mask = self.compute_valid_mask(torch.tensor([H, W]), inv_homography=torch.eye(3))
+        from utils.utils import compute_valid_mask
+        valid_mask = compute_valid_mask(torch.tensor([H, W]), inv_homography=torch.eye(3))
         input.update({'image': img_aug})
         input.update({'valid_mask': valid_mask})
 
@@ -443,7 +445,7 @@ class Coco(data.Dataset):
                     input.update({'warped_labels_bi': warped_labels_bi})
 
                 input.update({'warped_img': warped_img, 'warped_labels': warped_labels, 'warped_res': warped_res})
-
+                from utils.utils import compute_valid_mask
                 # print('erosion_radius', self.config['warped_pair']['valid_border_margin'])
                 valid_mask = self.compute_valid_mask(torch.tensor([H, W]), inv_homography=inv_homography,
                             erosion_radius=self.config['warped_pair']['valid_border_margin'])  # can set to other value

utils/cityscapes_camera.py
@@ -29,7 +29,7 @@ def load_cityscapes_camera(json_path):
 
     return K, R_cam, t
 
-def simulate_ego_motion(delta_yaw_deg=0.2, forward_cm=2.0, scale=0.3):
+def simulate_ego_motion(delta_yaw_deg=0.0, forward_cm=0.2, scale=0.3):
     delta_yaw_deg *= scale
     forward_cm *= scale
     R_delta = R.from_euler('y', np.deg2rad(delta_yaw_deg)).as_matrix()